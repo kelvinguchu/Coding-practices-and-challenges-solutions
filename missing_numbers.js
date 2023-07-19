6b6e389f542119472e093e499759c475b8b771fd
kelvinguchu <kelvinguchu5@gmail.com>
2023-07-19 14:19:49 +0300

    a function that checks if numbes in a given array add up to a given target sum.

---
+++ missing_numbers.js
@@ -0,0 +1,13 @@
+//a function that finds the missing number in an array of integers from 1 to 100.
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20];
+function findMissingNumber(array) {
+    for (let i = 1; i <= 100; i++) {
+        if (!array.includes(i)) {
+            return i;
+        };
+    };
+    return 0;
+}
+
+console.log(findMissingNumber(numbers));
