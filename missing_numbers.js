2a9f4d28ce7163c297957b4a777c1031aa599f90
kelvinguchu <kelvinguchu5@gmail.com>
2023-07-19 14:25:04 +0300

    a function that finds the missing number in an array of integers from 1 to 100.

--- missing_numbers.js
+++ missing_numbers.js
@@ -1,6 +1,6 @@
 //a function that finds the missing number in an array of integers from 1 to 100.
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12];
 function findMissingNumber(array) {
     for (let i = 1; i <= 100; i++) {
         if (!array.includes(i)) {
