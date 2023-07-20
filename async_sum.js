6720594fedc11ef7928b74c20ef6957215d091a6
kelvinguchu <kelvinguchu5@gmail.com>
2023-07-20 10:09:57 +0300

    an asychronous function to add 2 numbers and another one to multiply 2 numbers

---
+++ async_sum.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/*
+  Asynchronous function to add two numbers.
+  This function returns a Promise that resolves to the sum of 5 + 5.
+*/
+async function addNumbers() {
+  // Create a new Promise that immediately resolves to the sum of 5 + 5
+  let myPromise = new Promise((resolve, reject) => {
+    resolve(5 + 5);
+  });
+  // Wait for the Promise to resolve and return the result
+  return await myPromise;
+}
+
+// Call the addNumbers function and log the result when it resolves
+addNumbers().then(value => console.log(value));
+
+
+/*
+  Function to multiply two numbers.
+  This function returns a Promise that resolves to the product of 5 * 5.
+*/
+async function multiplyNumbers() {
+  // Create a new Promise that immediately resolves to the product of 5 * 5
+  let myPromise = new Promise((resolve, reject) => {
+    resolve(5 * 5);
+  });
+  // Wait for the Promise to resolve and return the result
+  return await myPromise;
+}
+
+// Call the multiplyNumbers function and log the result when it resolves
+multiplyNumbers().then(value => console.log(value));
