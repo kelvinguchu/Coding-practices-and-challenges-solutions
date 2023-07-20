d179a9edab788bce2c888754067c75c265566f50
kelvinguchu <kelvinguchu5@gmail.com>
2023-07-20 09:33:00 +0300

    A function that gives the sum of even numbers in an array.

---
+++ sum_even_nos.js
@@ -0,0 +1,20 @@
+'use strict';
+
+//A function that gives the sum of even numbers in an array.
+
+//Declare an array of numbers.
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/*Define a fuction that uses for_of loop
+to iterate over the numbers in the array*/
+function findSum(){
+    let sum = 0; // Initialise the sum to 0.
+    for(const x of numbers){
+        if( x % 2 == 0){
+            sum += x;
+        };
+    };
+    return sum;
+};
+
+console.log(findSum());
