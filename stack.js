581809874827c3b63adbbde7479b36cd246b1691
kelvinguchu <kelvinguchu5@gmail.com>
2023-09-01 19:40:31 +0300

    stack data structure implementation

--- stack.js
+++ stack.js
@@ -1,3 +1,5 @@
+'use strict'
+
 /**
  * Stack is a linear data structure which follows a particular order in which the
  * operations are performed. The order may be LIFO (Last In First Out) or FILO
