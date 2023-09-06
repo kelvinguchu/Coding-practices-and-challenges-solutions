7d719596a864f33ea0eefc5135e11a9f888b002b
kelvinguchu <kelvinguchu5@gmail.com>
2023-09-06 19:17:16 +0300

    a function to flatten a gien array of arrays

--- array_flattener.js
+++ array_flattener.js
@@ -1,18 +1,24 @@
+"use strict";
+
 //a function that takes in a nested array and returns a single array with all values flattened.
 //It should work for an array of arbitrary depth.
 
-let array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+let array = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
 
 function flatten(array) {
-    let result = [];
-    for (let i = 0; i < array.length; i++) {
-        if (Array.isArray(array[i])) {
-            result = result.concat(flatten(array[i]));
-        } else {
-            result.push(array[i]);
-        }
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      result = result.concat(flatten(array[i]));
+    } else {
+      result.push(array[i]);
     }
-    return result;
-};
+  }
+  return result;
+}
 
 console.log(flatten(array));
