30bbd228992b4cdefab17f115b9edb1ad50158e1
kelvinguchu <kelvinguchu5@gmail.com>
2023-09-06 19:19:11 +0300

    binary search algorithm implementation

--- binary_search.js
+++ binary_search.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /**
  * Binary search algorithm.
  */
@@ -11,29 +13,29 @@
  * @returns {number} The index of target in the array or -1 if target is not found.
  */
 function binarySearch(arr, target) {
-    let left = 0;           // Starting index of the array.
-    let right = arr.length - 1;  // Ending index of the array.
+  let left = 0; // Starting index of the array.
+  let right = arr.length - 1; // Ending index of the array.
 
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2); // Middle index of the current segment.
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2); // Middle index of the current segment.
 
-        // Check if target is present at mid.
-        if (arr[mid] === target) {
-            return mid;
-        }
-
-        // If target is greater than the middle element, it can only be present in the right subarray.
-        if (arr[mid] < target) {
-            left = mid + 1;
-        }
-        // Otherwise, target can only be present in the left subarray.
-        else {
-            right = mid - 1;
-        }
+    // Check if target is present at mid.
+    if (arr[mid] === target) {
+      return mid;
     }
 
-    // Target was not found in the array.
-    return -1;
+    // If target is greater than the middle element, it can only be present in the right subarray.
+    if (arr[mid] < target) {
+      left = mid + 1;
+    }
+    // Otherwise, target can only be present in the left subarray.
+    else {
+      right = mid - 1;
+    }
+  }
+
+  // Target was not found in the array.
+  return -1;
 }
 
 // Usage example:
@@ -42,7 +44,7 @@ const target = 7;
 
 const result = binarySearch(array, target);
 if (result !== -1) {
-    console.log(`Element ${target} is present at index ${result}`);
+  console.log(`Element ${target} is present at index ${result}`);
 } else {
-    console.log(`Element ${target} is not present in the array`);
+  console.log(`Element ${target} is not present in the array`);
 }
